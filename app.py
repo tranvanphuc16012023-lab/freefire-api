5eba032833ba2cfcbb51ddd6762b675ac64c5c8e
tranvanphuc16012023-lab <tranvanphuc16012023@gmail.com>
2025-09-14 09:44:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from datetime import datetime
 import hashlib
+import os
 
 app = Flask(__name__)
 
@@ -71,5 +72,7 @@ def get_stats():
         "server": "VN"
     })
 
+# QUAN TRỌNG: THÊM ĐOẠN NÀY Ở CUỐI
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
